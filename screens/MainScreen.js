c94d7f1776dd451c4daa5a060d90da36a2de4c8f
woic-ej <woic0102@naver.com>
2024-03-08 18:07:44 +0900

    Todo-app 만들기

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,14 +1,92 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import InputForm from "../components/InputForm";
+import TodoItem from "../components/TodoItem";
+import { useSelector } from "react-redux";
 
 const MainScreen = () => {
+  const todos = useSelector((state) => state.todo.todos);
+  const todoTasks = todos.filter((item) => item.state === "todo");
+  const completedTasks = todos.filter((item) => item.state === "done");
+
   return (
-    <View>
-      <Text>MainScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={"default"} />
+      <Text style={styles.pageTitle}>ToDo app</Text>
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>할 일</Text>
+        {todoTasks.length !== 0 ? (
+          <FlatList
+            data={todoTasks}
+            renderItem={({ item }) => <TodoItem {...item} />}
+            keyExtractor={(item) => item.id.toString()} // item.id를 문자열로 변환해야 합니다.
+          />
+        ) : (
+          <Text style={styles.emptyListText}>할 일이 없습니다.</Text>
+        )}
+      </View>
+      <View style={styles.separator} />
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>완료된 일</Text>
+        {completedTasks.length !== 0 ? (
+          <FlatList
+            data={completedTasks}
+            renderItem={({ item }) => <TodoItem {...item} />}
+            keyExtractor={(item) => item.id.toString()} // item.id를 문자열로 변환해야 합니다.
+          />
+        ) : (
+          <Text style={styles.emptyListText}>완료된 일이 없습니다.</Text>
+        )}
+      </View>
+      <InputForm />
+    </SafeAreaView>
   );
 };
 
 export default MainScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? 20 : 0,
+    backgroundColor: "#f7f8fa",
+  },
+  pageTitle: {
+    marginBottom: 35,
+    paddingHorizontal: 15,
+    fontSize: 53,
+    fontWeight: "600",
+  },
+  separator: {
+    marginHorizontal: 10,
+    marginTop: 20,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "rgba(0,0,0,0.2)",
+  },
+  listView: {
+    flex: 1,
+  },
+  listTitle: {
+    marginBottom: 25,
+    paddingHorizontal: 15,
+    fontSize: 41,
+    fontWeight: "500",
+  },
+  emptyListText: {
+    paddingTop: 10,
+    paddingBottom: 15,
+    paddingHorizontal: 15,
+    fontSize: 15,
+    lineHeight: 20,
+    color: "#737373",
+  },
+});
